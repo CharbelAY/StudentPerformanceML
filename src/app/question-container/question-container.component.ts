04540e4680de2b28891a0160e5cc93eeadddf0e3
Charbelay <charbel@abouyounes.com>
2019-09-22 21:29:20 +0300

    fixed problem of undefined variables

--- src/app/question-container/question-container.component.ts
+++ src/app/question-container/question-container.component.ts
@@ -7,18 +7,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QuestionContainerComponent implements OnInit {
 
-  typesOfShoes: string[] = [
-    "More than one of my grandparents has a university degree",
-    "One of my grandparents has a university degree",
-    "Both my parents have a university degree",
-    "One of my parents has a university degree",
-    "None of my parents have a university degree",
+    Questionaire:Array<Object>= [
+    {
+      'question':"Which statement is true ?",
+      'answers':[
+        "More than one of my grandparents has a university degree",
+        "One of my grandparents has a university degree",
+        "Both my parents have a university degree",
+        "One of my parents has a university degree",
+        "None of my parents have a university degree",
+      ]
+    },
+    {
+      'question':"Do you have older siblings?  ",
+      'answers':[
+        "Yes",
+        "No"
+      ]
+    },
   ];
 
-
+  loge(selected){
+    console.log(selected);
+  }
   constructor() { }
 
   ngOnInit() {
   }
-
 }
